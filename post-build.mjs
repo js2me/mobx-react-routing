bcaf34383653c806da7637321e90e110054d13af
Sergey Volkov <js2me@outlook.com>
2024-11-04 12:43:14 +0300

    internal: improve publish script

--- post-build.mjs
+++ post-build.mjs
@@ -1,8 +1,24 @@
-import { postBuildScript } from 'js2me-exports-post-build-script';
+import { postBuildScript, publishScript } from 'js2me-exports-post-build-script';
 
 postBuildScript({
   buildDir: 'dist',
   rootDir: '.',
   srcDirName: 'src',
   filesToCopy: ['LICENSE', 'README.md'],
+  updateVersion: process.env.PUBLISH_VERSION,
+  onPackageVersionChanged: (nextVersion, currVersion) => {
+    if (process.env.PUBLISH) {
+      publishScript({
+        nextVersion,
+        currVersion,
+        publishCommand: 'pnpm publish',
+        commitAllCurrentChanges: true,
+        createTag: true,
+        githubRepoLink: 'https://github.com/js2me/mobx-react-routing',
+        cleanupCommand: 'pnpm clean', 
+      })
+    }
+  }
 });
+
+
